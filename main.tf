c773c42285add6bc979c7d61b68e478091f4dc72
Felipe <fevabac@alumni.uv.es>
2024-03-24 12:40:04 +0100

    Actualización de identación

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-# Creación de red, subred y router
-# See:
-# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_network_v2
-# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_subnet_v2
-# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_router_v2  https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_router_interface_v2
-#
 module "networking"{
   source = "./modules/networking"
   ext_net_name = var.external_network_name
@@ -12,55 +6,34 @@ module "networking"{
   router_name = var.router_name
 }
 
-# # Creación del grupo de seguridad
-# # See:
-# # https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_secgroup_v2
-# module "secgroup"{
-#    source = "./modules/secgroup"
-#    secgroup_name = var.security_group_name
-# }
+module "secgroup"{
+    source = "./modules/secgroup"
+    secgroup_name = var.security_group_name
+}
 
-# # Creación de una regla en el grupo de seguridad
-# # See:
-# # https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_secgroup_rule_v2
-# module "secgroup_rule_http"{
-#    source = "./modules/secgroup_rule"
-#    secgroup_id = module.secgroup.security_group_id
-#    secgroup_rule_name = var.sg_rule_name
-#    port_ini = var.port_ini
-#    port_fin = var.port_fin
-# }
+module "secgroup_rule_http"{
+    source = "./modules/secgroup_rule"
+}
 
-# # Creación de un servidor con nginx en la red creada (donde estará el balanceador de carga)
-# # See
-# # https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/compute_instance_v2
-# module "server1" {
-#   source = "./modules/server"
-#   server_name = var.server1_name
-#   node_number = "0"
-#}
+module "server0" {
+   source = "./modules/server"
+   server_name = var.server0_name
+   node_number = "0"
+}
 
-# # Creación de un servidor con nginx en la red creada (donde estará el balanceador de carga)
-# # See
-# # https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/compute_instance_v2
-# module "server1" {
-#   source = "./modules/server"
-#   server_name = var.server2_name
-#   node_number = "1"
-#}
+module "server1" {
+   source = "./modules/server"
+   server_name = var.server1_name
+   node_number = "1"
+}
 
-# # Creación de un servidor con nginx en la red creada (donde estará el balanceador de carga)
-# # See
-# # https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/compute_instance_v2
-# module "server1" {
-#   source = "./modules/server"
-#   server_name = var.server3_name
-#   node_number = "2"
-#}
+module "server2" {
+   source = "./modules/server"
+   server_name = var.server2_name
+   node_number = "2"
+}
 
-# # Reservar una IP flotante para el proyecto:
-# # See https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_floatingip_v2
-# module "flotaing_ip" {
-#   source = "./modules/flotaing_ip"
-#   ext_net_name=var.external_network_name
-# }
+module "flotaing_ip" {
+   source = "./modules/flotaing_ip"
+   ext_net_name=var.external_network_name
+}
--- variables.tf
+++ variables.tf
@@ -4,3 +4,20 @@ variable subnetwork_name {}
 variable router_name {}
 
 # Declaración del resto de variables
+variable server0_name {
+    description = "The name of the server"
+    type        = string
+    default     = "Node0"
+}
+
+variable server1_name {
+    description = "The name of the server"
+    type        = string
+    default     = "Node1"
+}
+
+variable server2_name {
+    description = "The name of the server"
+    type        = string
+    default     = "Node2"
+}
